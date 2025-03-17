16dc875e8c1fdd56e4daa91794c41fb5a03b0f02
tabilzad <tim.abilzade@gmail.com>
2025-03-16 20:44:22 -0400

    update to ktor 3.1.0 add global security example

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import io.github.tabilzad.ktor.model.SecurityScheme
+
 plugins {
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
-    id("io.ktor.plugin") version "2.3.12"
-    id("io.github.tabilzad.ktor-docs-plugin-gradle") version "0.6.2-alpha"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.serialization") version "2.1.10"
+    id("io.ktor.plugin") version "3.1.1"
+    id("io.github.tabilzad.inspektor") version "0.7.2-alpha"
 }
 
 group = "com.example"
@@ -10,9 +12,44 @@ version = "0.0.1"
 
 swagger {
     documentation {
-        docsTitle = "Example Ktor Server"
-        docsDescription = "Example Server Description"
-        docsVersion = "1.0"
+        servers = listOf("http://localhost:8080", "http://127.0.0.1:8080")
+        info {
+            title = "Example Ktor Server"
+            description = "Example Server Description"
+            version = "10"
+
+            contact {
+                name = "Inspektor"
+                url = "https://github.com/tabilzad/ktor-docs-plugin"
+            }
+        }
+        security {
+            scopes {
+                or {
+                    +"BasicAuth"
+                    and {
+                        "OpenId" to listOf("admin", "temp")
+                    }
+                }
+            }
+            schemes {
+                "BasicAuth" to SecurityScheme(
+                    type = "http",
+                    scheme = "basic",
+                    description = "Authenticate using basic username/password credentials"
+                )
+                "Bearer" to SecurityScheme(
+                    type = "http",
+                    scheme = "bearer",
+                )
+                "ApiKeyAuth" to SecurityScheme(
+                    type = "apiKey",
+                    `in` = "header",
+                    name = "X-API-Key"
+                )
+            }
+
+        }
     }
 
     pluginOptions {
